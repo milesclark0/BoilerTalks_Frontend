0410ab420bd215ef105aa8867b7a946516c8c02d
wang4621 <82539201+wang4621@users.noreply.github.com>
2023-02-05 22:29:48 -0500

    added multiple inputs

--- src/component/Register/JoinThreads.tsx
+++ src/component/Register/JoinThreads.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Autocomplete, TextField, Checkbox, FormHelperText, InputAdornment } from "@mui/material";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import WarningIcon from "@mui/icons-material/Warning";
 
 const JoinThreads = ({ setActiveStep }) => {
+  const courses = ["CS180", "CS182", "CS240", "CS250", "CS251", "CS252", "CS307", "CS348", "CS407", "CS408", "CS381", "CS448"];
+  const [selectedCourses, setSelectedCourses] = useState([]);
+  const [error, setError] = useState(false);
+
   const handleBack = () => {
     setActiveStep((prevActiveStep: number) => prevActiveStep - 1);
   };
@@ -11,13 +18,34 @@ const JoinThreads = ({ setActiveStep }) => {
   };
 
   const handleNext = () => {
-    setActiveStep((prevActiveStep: number) => prevActiveStep + 1);
+    if (selectedCourses.length !== 0) {
+      setActiveStep((prevActiveStep: number) => prevActiveStep + 1);
+    } else {
+      setError(true);
+    }
   };
 
   return (
     <Box>
-      <Box>
-
+      <Box sx={{ mt: 3 }}>
+        <Autocomplete
+          multiple
+          id="selectCourses"
+          options={courses}
+          disableCloseOnSelect
+          renderOption={(props, option, { selected }) => (
+            <li {...props}>
+              <Checkbox icon={<CheckBoxOutlineBlankIcon />} checkedIcon={<CheckBoxIcon />} style={{ marginRight: 8 }} checked={selected} />
+              {option}
+            </li>
+          )}
+          onChange={(event, value) => setSelectedCourses(value)}
+          style={{ width: 500 }}
+          renderInput={(params) => <TextField {...params} label="Select Courses" placeholder="Courses" />}
+        />
+        <FormHelperText sx={{ fontSize: "14px", marginLeft: 0 }} error={error}>
+          {error ? "If you click next, you will need to select course(s). Else just press skip." : ""}
+        </FormHelperText>
       </Box>
       <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
         <Button color="inherit" sx={{ mr: 1 }} onClick={handleBack}>

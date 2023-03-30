9a864695e48ab75e851b7625b92fa819ef444ec2
oneKZhang <zhan3461@purdue.edu>
2023-03-30 05:41:02 -0400

    added api calls for requests

--- src/API/CourseManagementAPI.tsx
+++ src/API/CourseManagementAPI.tsx
@@ -14,6 +14,10 @@ export const updateWarnListURL = "/courseManagement/updateWarnList/";
 
 export const updateCourseRulesURL = "/courseManagement/updateCourseRules/";
 
-export const addReportURL = "/courseManagement/addReport/"
+export const addReportURL = "/courseManagement/addReport/";
 
-export const removeReportURL = "/courseManagement/removeReport/"
+export const removeReportURL = "/courseManagement/removeReport/";
+
+export const addRequestURL = "/courseManagement/addRequest/";
+
+export const removeRequestURL = "/courseManagement/removeRequest/";

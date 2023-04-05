4f642e19c1465b4a34f61bcc2ee8f099baa30ae5
wang4621 <82539201+wang4621@users.noreply.github.com>
2023-04-04 22:05:41 -0400

    moved items in settings

---
+++ src/component/Settings/NotificationPreference.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useAuth } from "../../context/context";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import { useState } from "react";
+import {
+  Alert,
+  AppBar,
+  Box,
+  Button,
+  Drawer,
+  FormHelperText,
+  FormLabel,
+  IconButton,
+  InputAdornment,
+  Paper,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+const NotificationPreference = () => {
+  return (
+    <div>NotificationPreference</div>
+  )
+}
+
+export default NotificationPreference
